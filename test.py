71ab5db2b7c1eef29a0ad2a27434e58020975035
Jake VanderPlas <jakevdp@gmail.com>
2016-06-16 09:35:35 -0700

    change test.py to using lists

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ def print_hello_world():
 
 
 def fib(n):
+    fibs = []
     a, b = 0, 1
     for i in range(n):
         a, b = b, a + b
-        yield a
+        fibs.append(a)
+    return fibs
 
 print_hello_world()
 print(list(fib(10)))
